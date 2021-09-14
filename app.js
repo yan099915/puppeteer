5995412b89aef9ff41beb1c4578f79db964cb4de
unknown <nganggur.pisan@gmail.com>
2021-09-14 16:40:18 +0700

    init

---
+++ app.js
@@ -0,0 +1,36 @@
+const puppeteer = require("puppeteer");
+const application = require("./downloader");
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1280, height: 800 });
+  await page.goto(
+    "https://www.youtube.com/c/RoyaltyFreeMusicNoCopyrightMusic/videos"
+  );
+  await page.evaluate((_) => {
+    window.scrollBy(1, window.innerHeight);
+  });
+
+  let videoThumbnail = await page.$$("#items #dismissible");
+  for (i = 1; i <= videoThumbnail.length; i++) {
+    if (i == videoThumbnail.length) {
+      page.evaluate((_) => {
+        window.scrollBy(5, window.innerHeight);
+      });
+      await page.waitForTimeout(10000);
+      videoThumbnail = await page.$$("#items #dismissible");
+      console.log(videoThumbnail.length);
+    }
+    const videoTitle = await page.$$eval(
+      `#items ytd-grid-video-renderer:nth-child(${i}) #dismissible #details #meta #video-title`,
+      (nodes) => nodes.map((n) => n.href)
+    );
+
+    console.log(i);
+    console.log(videoTitle);
+
+    const download = await application.download(videoTitle);
+  }
+  //   await browser.close();
+})();
---
+++ downloader.js
@@ -0,0 +1,41 @@
+const ytdl = require("ytdl-core");
+const fs = require("fs");
+const path = require("path");
+
+module.exports = {
+  download: async (url) => {
+    const videoID = url.toString().split("=");
+
+    let info = await ytdl.getInfo(videoID[1]);
+    let audioFormats = ytdl.filterFormats(info.formats, "audioonly");
+    const filename = info.videoDetails.title.toString().split("|");
+
+    // make directory for downloaded assets
+    const directory = await fs.mkdir(
+      "./downloaded/" + filename[0] + "/",
+      { recursive: true },
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    // save downloaded file information to json file
+    const jsonfile = await fs.writeFile(
+      path.resolve(`./downloaded/${filename[0]}/${filename[0]}.json`),
+      JSON.stringify(info),
+      function (err) {
+        if (err) {
+          console.log(err);
+        }
+      }
+    );
+
+    // save m4a file to folder
+    const download = await ytdl(url).pipe(
+      fs.createWriteStream(`./downloaded/${filename[0]}/${i}.m4a`)
+    );
+    return filename[0];
+  },
+};
